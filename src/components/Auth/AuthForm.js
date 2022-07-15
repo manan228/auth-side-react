9bc8e899c888438d3c0e7832922ab9dbdc711f81
manan228 <manan228@yahoo.in>
2022-07-15 13:18:39 +0530

    auth login completed

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,6 +1,6 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -14,62 +14,93 @@ const AuthForm = () => {
   };
 
   const submitHandler = (event) => {
-
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
-    setIsLoading(true)
+    setIsLoading(true);
 
-    if(isLogin) {
-
-    } else {
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAMS1m2_2WgT8zRzPPjVbiC7Oibr9Zzzy0', {
-        method: 'POST',
-        body: JSON.stringify({
-
-          email: enteredEmail,
-          password: enteredPassword,
-          returnSecureToken: true
-        }),
-        headers: {
-          'Content-Type': 'application/json'
+    if (isLogin) {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAMS1m2_2WgT8zRzPPjVbiC7Oibr9Zzzy0",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      }).then(res => {
+      ).then((res) => {
+
         if(res.ok) {
-          setIsLoading(false)
+          console.log(res)
         } else {
-          return res.json().then(data => {
-            console.log(data)
-            setIsLoading(false)
-            alert(data.error.message)
-          })
+          alert(`login failed`)
         }
       })
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAMS1m2_2WgT8zRzPPjVbiC7Oibr9Zzzy0",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+          setIsLoading(false);
+        } else {
+          return res.json().then((data) => {
+            console.log(data);
+            setIsLoading(false);
+            alert(data.error.message);
+          });
+        }
+      });
     }
-  }
+  };
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required  ref={emailInputRef}/>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required ref={passwordInputRef}/>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : isLoading ? 'sending request...' : 'Create Account'}</button>
-          {/* <button>{}</button> */}
+          <button>
+            {isLogin
+              ? "Login"
+              : isLoading
+              ? "sending request..."
+              : "Create Account"}
+          </button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
